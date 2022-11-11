78a7e889b2d2c16aa5488d74ed31857c93c914a1
fhoefler@htl-steyr.ac.at <!"Barcelona29>
2022-11-11 11:34:58 +0100

    ProduktController liste ausgeben

--- src/Controller/ProduktController.php
+++ src/Controller/ProduktController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Department;
 use App\Entity\Produkt;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,10 +12,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProduktController extends AbstractController
 {
     #[Route('/produkt', name: 'app_produkt')]
-    public function index(): Response
+    public function index(ManagerRegistry $registry): Response
     {
         $data = null;
-        $data = $this->getDoctrine()->getRepository(Produkt::class)->findAll();
+        $data = $registry->getManager()->getRepository(Produkt::class)->findAll();
+
 
         return $this->render('Rechnungsgen/Product.html.twig', [
             'car_form' => $data
