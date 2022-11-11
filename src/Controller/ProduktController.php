afb4f60aea7a7b442e4f7465c5b5276a14838ff6
mdramac <mdramac@htl-steyr.ac.at>
2022-11-11 11:08:46 +0100

    Produkte auflisten

--- src/Controller/ProduktController.php
+++ src/Controller/ProduktController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Produkt;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +12,12 @@ class ProduktController extends AbstractController
     #[Route('/produkt', name: 'app_produkt')]
     public function index(): Response
     {
+        $data = null;
+        $data = $this->getDoctrine()->getRepository(Produkt::class)->findAll();
+
         return $this->render('Rechnungsgen/Product.html.twig', [
-            'controller_name' => 'ProduktController',
+            'car_form' => $data
         ]);
+
     }
 }
