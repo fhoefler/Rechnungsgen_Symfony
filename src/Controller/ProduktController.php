9e0c80e7fee8d752ae6545f49b3ae1bbd25d36f0
fhoefler@htl-steyr.ac.at <!"Barcelona29>
2022-11-11 11:20:44 +0100

    twig

--- src/Controller/ProduktController.php
+++ src/Controller/ProduktController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Produkt;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,13 +11,9 @@ class ProduktController extends AbstractController
     #[Route('/produkt', name: 'app_produkt')]
     public function index(): Response
     {
-        $data = null;
-        $data = $this->getDoctrine()->getRepository(Produkt::class)->findAll();
-
         return $this->render('Rechnungsgen/Product.html.twig', [
-            'car_form' => $data
+            'controller_name' => 'ProduktController',
         ]);
-
     }
 
 
