96d5405eeaaa6c4a9474d250543bc972d187bf9b
mdramac <mdramac@htl-steyr.ac.at>
2022-11-18 10:37:05 +0100

    creat customer

--- src/Controller/KundenController.php
+++ src/Controller/KundenController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Kunden;
+use http\Env\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,15 +13,42 @@ class KundenController extends AbstractController
     #[Route('/kunden', name: 'app_kunden')]
     public function index(): Response
     {
-        return $this->render('Rechnungsgen/Kunden.html.twig', [
-            'controller_name' => 'KundenController',
+        $data = null;
+        $data = $this->getDoctrine()->getRepository(Kunden::class)->findAll();
+
+        return $this->render('Rechnungsgen/Product.html.twig', [
+            'car_form' => $data
         ]);
     }
 
 
     #[Route('/createkunden', name: 'app_createkunden')]
-    public function create(): Response
+    public function create(Request $request): Response
     {
+        if ($request->getMethod() === "POST") {
+
+            /**
+             * @Var Kunden $kunden
+             */
+
+            $kunden = new Kunden();
+            $kunden->setName($request->get("name"));
+            $kunden->setEmail($request->get("email"));
+            $kunden->setTel($request->get("telnr"));
+            $kunden->setAddresse($request->get("adress"));
+            $kunden->setFirma(1);
+            $kunden->setKundennummer($request->get("kundnr"));
+            $kunden->setPlz($request->get("plz"));
+            $kunden->setStadt($request->get("city"));
+            $kunden->setUid($request->get("uid"));
+            $kunden->setFntz($request->get("fest"));
+            $kunden->setFn($request->get("fn"));
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($kunden);
+            $entityManager->flush();
+            return $this->render('Rechnungsgen/index.html.twig');
+        }
         return $this->render('Rechnungsgen/Create/CreateCustomer.html.twig', [
             'controller_name' => 'KundenController',
         ]);
